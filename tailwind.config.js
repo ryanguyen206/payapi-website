ea0b93b20fd75fa564da711a30f94bc6cd06ca55
ryanguyen206 <ryanninja595@gmail.com>
2024-03-08 19:47:12 -0800

    Customize colors, add font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary_dark: '#ba4270',
+        primary_light: '#fbfcfe',
+        secondary_blue: '#36536b',
+        secondary_mirage:'#1b262f',
+        secondary_pink:'#da6d97',
+        secondary_lblue:'#6c8294'
+      },
+      fontFamily:{
+        sans:['Public Sans', 'sans-serif'],
+        display:['DM Serif Display', 'sans-serif']
+      }
+   
   },
   plugins: [],
+  }
 }
